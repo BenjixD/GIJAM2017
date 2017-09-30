09943ade5a45dc8f404044abfdd3196c5fea787c
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2017-09-30 18:05:35 -0400

    Clouds blowing animation

---
+++ Assets/moveClouds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveClouds : MonoBehaviour {
+
+	public float xSpeed;
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody2D>().velocity = new Vector3 (-xSpeed, 0, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
