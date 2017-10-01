bec71804948a5362f10a4f38a442c5b01b604398
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2017-10-01 15:32:20 -0400

    Changing background

---
+++ Assets/ChangeBackgroundColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBackgroundColor : MonoBehaviour {
+
+    public GlobalCount Counter;
+
+    SpriteRenderer anim;
+    Color myColor;
+
+    public Color TargetColor;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<SpriteRenderer>();
+        myColor = anim.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.color = Color.Lerp(myColor, TargetColor, ((float)(Counter.totalBullets))/100);
+    }
+}
